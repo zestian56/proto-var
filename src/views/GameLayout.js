2692319634bdd0bae25f584fedfff97ffe2ae83b
zestian56 <sebas-565@hotmail.com>
2018-11-01 13:18:08 -0500

    Commit inicial

---
+++ src/views/GameLayout.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import * as classes from  './GameLayout.module.scss';
+
+class GameLayout extends Component {
+    render() {
+        return (
+            <div className={classes.container}>
+                
+            </div>
+        );
+    }
+}
+
+export default GameLayout;
